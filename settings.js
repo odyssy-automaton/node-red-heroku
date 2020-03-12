7a84def4a916ceccdab11373a30bf8838eb54721
Dekan <dekanbro@gmail.com>
2020-03-12 09:57:01 -0600

    use connectMode

--- settings.js
+++ settings.js
@@ -56,6 +56,7 @@ var settings = module.exports = {
         telegram: {
           'caletaXP_bot': {
             token: process.env.NODE_RED_TOKEN,
+            connectMode: 'polling',
             polling: 1000,
             parseMode: 'markdown',
           }
